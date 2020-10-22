9fafda951fa3a5eb7477db3cea4a33e96be4761e
iv <vifs888@gmail.com>
2020-10-22 10:44:55 -0600

    another small bug fix

--- 12thMorning/12thMorning/Models/Queslar/Enchanting/EnchantServiceInfo.cs
+++ 12thMorning/12thMorning/Models/Queslar/Enchanting/EnchantServiceInfo.cs
@@ -37,7 +37,10 @@ namespace _12thMorning.Models.Queslar.Enchanting {
 
         public static List<EnchantServiceInfo> Build(double avg, int level, int cost) {
             var temp = (int) Math.Round(Math.Sqrt(level) * 4 * 3);
-            var lowerLevel = (int)Math.Round(Math.Pow((temp - .5) / 3 / 4.0, 2)) + 1;
+            var lowerLevel = (int)Math.Round(Math.Pow((temp - .5) / 3 / 4.0, 2));
+            if(lowerLevel != temp) {
+                lowerLevel++;
+            }
             var upperLevel = (int)Math.Round(Math.Pow((temp + .5) / 3 / 4.0, 2)) + 1;
             var returnList = new List<EnchantServiceInfo>();
             returnList.Add(new EnchantServiceInfo(avg, level, cost));
