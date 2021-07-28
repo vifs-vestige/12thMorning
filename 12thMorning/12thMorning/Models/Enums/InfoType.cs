2cc8119064d1f8d12d1fee3171f395b13c53f85b
vifs <vifs888@gmail.com>
2021-07-27 22:30:06 -0600

    added about page with links and stuff

---
+++ 12thMorning/12thMorning/Models/Enums/InfoType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _12thMorning.Models.Enums {
+    public enum InfoType {
+        about, links, faq, epithets
+    }
+}
