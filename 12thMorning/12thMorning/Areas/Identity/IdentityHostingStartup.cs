a77a83e02b6928cd61498f1b2af9740870936bb0
vifs <vifs888@gmail.com>
2021-10-27 21:52:16 -0600

    got roles working to some degree

--- 12thMorning/12thMorning/Areas/Identity/IdentityHostingStartup.cs
+++ 12thMorning/12thMorning/Areas/Identity/IdentityHostingStartup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using _12thMorning.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -14,20 +15,55 @@ namespace _12thMorning.Areas.Identity
     {
         public void Configure(IWebHostBuilder builder)
         {
-            
+
 
             var temp = ServerVersion.AutoDetect("Server=localhost;Database=12thmorning;Uid=12thmorning;");
-            builder.ConfigureServices((context, services) => {
+            builder.ConfigureServices((context, services) =>
+            {
                 services.AddDbContext<_12thMorningContext>(options =>
-                options.UseMySql(@"Server = localhost; Database = 12thmorning; Uid = 12thmorning;", temp));
+                    options.UseMySql(@"Server = localhost; Database = 12thmorning; Uid = 12thmorning;", temp)
+                    );
 
 
                 services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                     .AddRoles<IdentityRole>()
                     .AddEntityFrameworkStores<_12thMorningContext>();
+
+                CreateRoles(services.BuildServiceProvider());
+
+
             });
 
 
+
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleNames = new string[]{ "admin" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExists = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExists)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            var powerUser = await UserManager.FindByNameAsync("vifs_vestige");
+            var temp = UserManager.GetRolesAsync(powerUser);
+            if (powerUser != null)
+            {
+                await UserManager.AddToRoleAsync(powerUser, "admin");
+            }
+
+            var temp2 = await UserManager.GetRolesAsync(powerUser);
+            //await UserManager.GetRolesAsync(powerUser);
+            var temp3 = "a";
         }
     }
 }
